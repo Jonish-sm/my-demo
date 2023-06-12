e1c494f56bfb4d70ccbad30d01e3e9c241bad334
Jonish-sm <jonishsubba@gmail.com>
2023-06-12 11:41:02 +0530

    Request to check the error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform"
 import About from "./components/About";
@@ -6,9 +6,19 @@ import About from "./components/About";
 
 
 function App() {
+  const[mode, setMode]= useState('dark'); //whether dark mode is enable or not
+  const toggleMode = ()=>{
+    if(mode ==='light'){
+      setMode = 'dark'
+    }else{
+      setMode = 'light'
+    }
+    
+  }
+
   return (
     <>
-     <Navbar title="React-Tutorial" aboutText="About" />
+     <Navbar title="React-Tutorial" mode={mode} aboutText="About" toggleMode={toggleMode} />
      <div className="container my-3">
       
      <Textform heading="Enter the text to analyze below"/> 
